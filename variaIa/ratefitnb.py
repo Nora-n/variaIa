4a727b4651494717e2b41c1960e956a063e52e8f
Nora NICOLAS <nora.nicolas@ens-lyon.org>
2019-11-06 17:43:27 +0100

    Ratefitnb now uses poisson pmf. Includes AICc, /DoF, Chi2, /DoF. TBC.

--- variaIa/ratefitnb.py
+++ variaIa/ratefitnb.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from scipy import stats
 from astropy.cosmology import Planck15 as cosmo
 import matplotlib.pyplot as plt
 import iminuit as im
@@ -30,32 +31,60 @@ class RateFit():
     def get_cuts(self, zmax):
         '''Implémentation de zmax pour le fit'''
         zwhere = np.where(self.data < zmax)
-        return(self.data[zwhere], self.counts[zwhere])
+        res = {'dat': self.data[zwhere],
+               'counts': self.counts[zwhere]}
+        return(res)
 
     def get_ratemodel(self, z, a):
         ''' Le modèle en question'''
         return a/self.VOLUME_SCALE * cosmo.comoving_volume(z).value
 
+    def get_probabilities(self, a, zmax):
+        """ Returns the poisson statistics applied to each cases """
+        return stats.poisson.pmf(self.get_cuts(zmax)['counts'],
+                                 self.get_ratemodel(self.get_cuts(zmax)['dat'],
+                                                    a))
+
     def get_loglikelihood(self, a, zmax):
-        '''Donne le likelihood sur la totalité des données'''
-        return -np.sum((self.get_cuts(zmax)[1]
-                       - self.get_ratemodel(self.get_cuts(zmax)[0], a))**2)
+        """ """
+        # poisson.pmf(k, mu) = probability to observe k counts\
+        # given that we expect mu
+        return -np.sum(np.log(self.get_probabilities(a, zmax)))
+
+    def get_loglikeDOF(self, a, zmax):
+        """Test of better minimization function"""
+        k = len(self.FREEPARAMETERS)
+        n = len(self.get_cuts(zmax)['dat'])
+
+        DoF = n-k
+
+        return(self.get_loglikelihood(a, zmax)/DoF)
 
     def get_aicc(self, a, zmax):
         k = len(self.FREEPARAMETERS)
+        n = len(self.get_cuts(zmax)['dat'])
         logl = self.get_loglikelihood(a, zmax)
 
-        return 2*k + logl + (2*k*(k+1))/(len(self.get_cuts(zmax)[0])-k-1)
+        return(2*k + logl + (2*k*(k+1))/(n-k-1))
+
+    def get_aiccDOF(self, a, zmax):
+        """To have high zmax even with lots of data"""
+        k = len(self.FREEPARAMETERS)
+        n = len(self.get_cuts(zmax)['dat'])
+
+        DoF = n-k
+
+        return(self.get_aicc(a, zmax)/DoF)
 
     def get_logprior(self, a, zmax):
         '''Donne la probabilité du modèle (limites sur "a", "zmax")'''
         if np.logical_and(a > 1e-1,
-                          a < 1e1):
+                          a < 6e1):
             prior_bounds_a = 0
         else:
             prior_bounds_a = 1e10
 
-        if np.logical_and(zmax > 1e-1,
+        if np.logical_and(zmax >= 1e-1,
                           zmax < 7e-1):
             prior_bounds_zmax = 0
         else:
@@ -64,40 +93,51 @@ class RateFit():
         used_priors = [prior_bounds_a, prior_bounds_zmax]
         return np.sum(used_priors)
 
-    def get_logproba(self, a, zmax):
+    def get_logprob(self, a, zmax):
         """ """
-        return self.get_aicc(a, zmax) + self.get_logprior(a, zmax)
+        return self.get_loglikeDOF(a, zmax) + self.get_logprior(a, zmax)
 
     def minimize(self, **kwargs):
         '''Renvoie la meilleure valeur des paramètres'''
-        self.m = im.Minuit(self.get_logproba, **kwargs,
+        self.m = im.Minuit(self.get_logprob, **kwargs,
                            print_level=0, pedantic=False)
 
         self.migrad_out = self.m.migrad()
 
-        self.set_param([self.m.values['a'],
-                        self.m.values['zmax']])
+        self.set_param([round(self.m.values['a'], 4),
+                        round(self.m.values['zmax'], 4)])
 
 #   ################################### PLOTTER ###############################
 
-    def plot(self, label):
+    def plot(self, label, guess=None):
         '''Trace les données vs le modèle, et modèle fitté (optionnel)'''
         fig = plt.figure(figsize=[8, 5])
         ax = fig.add_axes([0.1, 0.12, 0.8, 0.8])
+
+        zz = np.linspace(0, np.max(self.data))
 
         ax.plot(self.data,
                 self.counts,
                 color='blue',
                 lw=1.0, label=label)
 
-        ax.plot(self.data,
-                self.get_ratemodel(self.data, self.param['a']),
+        ax.plot(zz,
+                self.get_ratemodel(zz, self.param['a']),
                 color='black',
                 lw=1.0, label='expected')
 
         ax.vline(self.param['zmax'],
                  color=".3",
                  lw=1.0, label="zmax")
+        if guess is not None:
+            ax.plot(zz,
+                    self.get_ratemodel(zz, guess[0]),
+                    color='orange',
+                    lw=1.0, label='expected_guess')
+
+            ax.vline(guess[-1],
+                     color="orange",
+                     lw=1.0, label="zmax_guess")
 
         zerr = self.migrad_out[1][1][3]
 
@@ -117,7 +157,7 @@ class RateFit():
         ax.set_xlabel(r'$\mathrm{redshift}$', fontsize='x-large')
         ax.set_ylabel(r'$\mathrm{N}_\mathrm{SNe\,Ia}$', fontsize='x-large')
 
-        plt.legend(ncol=1, loc='upper right')
+        plt.legend(ncol=1, loc='lower right')
 
         plt.title(r'$\mathrm{Evolution\,\,of\,\,poisson\,\,cdf\,\,with\,\,}$' +
                   r'$\mathrm{median\,\,for\,\,surveys}$',
